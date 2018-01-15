308640a8f8298e1c1c85be32060d66a904b4aa72
chriskilinc <chriskilinc@gmail.com>
2018-01-15 14:04:05 +0100

    Added a function to get recent posts from wp REST API and pass it down to Content Component

--- src/App.js
+++ src/App.js
@@ -8,17 +8,28 @@ import Content from './Components/Content';
 
 class App extends Component {
 
-  constructor(){
+  constructor() {
     super()
-
 
     this.currentDate = {
       full: new Date(),
-      time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}),
+      time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false }),
       year: new Date().getFullYear(),
-      monthString: new Date().toLocaleString('en-us', {  month: 'long' }),
+      monthString: new Date().toLocaleString('en-us', { month: 'long' }),
       date: new Date().getDate(),
-      dayString: new Date().toLocaleString('en-us', {  weekday: 'long' }),
+      dayString: new Date().toLocaleString('en-us', { weekday: 'long' }),
+    }
+
+    this.state = {
+      articles: {
+        title:{
+          rendered: ''
+        },
+        content:{
+          rendered: ''
+        },
+        id: undefined,
+      },
     }
   }
 
@@ -28,19 +39,38 @@ class App extends Component {
       () => this.refreshWindow(),
       600000
     );
+
+    this.getArticles();
+
   }
 
   //  Reloads the window
   refreshWindow = () => {
     //window.location.reload();
   }
-  
+
+  // Fetches 10 recent articles
+  getArticles = () => {
+    fetch("http://testpress.chriskilinc.com/wp-json/wp/v2/posts/")
+      .then(response => {
+        if (!response.ok) {
+          throw Error("Network request failed")
+        }
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          articles: data
+        })
+      })
+  }
+
 
   render() {
     return (
       <div className="App container-fluid row">
-        < Navigation currentDate = {this.currentDate}/>
-        < Content />
+        < Navigation currentDate={this.currentDate}  />
+        < Content articles={this.state.articles} />
         < Sidebar />
       </div>
     );
